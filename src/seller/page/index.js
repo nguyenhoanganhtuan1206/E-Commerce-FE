638a121fb4b039c8ed621b7159195ba7aa7c9163
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-11-10 22:26:14 +0700

    Fix some bugs and create common component for Tabs Header

--- src/seller/page/index.js
+++ src/seller/page/index.js
@@ -2,3 +2,4 @@ export { default as MainPageSeller } from "./MainPageSeller/MainPageSeller";
 export { default as DashboardSeller } from "./Dashboard/DashboardSeller";
 export { default as MyAdsPage } from "./MyAdsPage/MyAdsPage";
 export { default as SellerSignUpPage } from "./SellerSignUpPage/SellerSignUpPage";
+export { default as ManagementOrdersPage } from "./ManagementOrdersPage/ManagementOrdersPage";
--- src/shared/components/index.js
+++ src/shared/components/index.js
@@ -17,3 +17,4 @@ export { default as Pagination } from "./Pagination/Pagination";
 export { default as TagProduct } from "./TagProduct/TagProduct";
 export { default as ButtonQuantity } from "./ButtonQuantity/ButtonQuantity";
 export { default as ProductImageDisplay } from "./ProductImageDisplay/ProductImageDisplay";
+export { default as TabsHeader } from "./TabsHeader/TabsHeader";
